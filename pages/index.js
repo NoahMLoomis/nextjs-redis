fefcae1774a85c0c87a78ccb891b54a7408d1754
nloomis <noahmmLoomis@gmail.com>
2022-01-25 09:28:33 -0500

    Final things fixed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import CarForm from "../lib/CarForm";
+import SearchForm from "../lib/SearchForm"
+
 
 export default function Home() {
   return (
     <div className="p-4">
       <h1>Create your car!</h1>
       <CarForm />
+      <SearchForm />
     </div>
   )
 }
